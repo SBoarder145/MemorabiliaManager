45735ffcddd3033aeec2f3b64fcfd6a2a88a8a7b
DESKTOP-A4HIJU6\student <SBoarder@outlook.com>
2022-03-27 18:29:59 -0500

    Fixed Item FK issue

--- MemorabiliaManager.Data/Migrations/20220327230038_ItemFKFix.Designer.cs
+++ MemorabiliaManager.Data/Migrations/20220327230038_ItemFKFix.Designer.cs
@@ -3,6 +3,7 @@ using MemorabiliaManager.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MemorabiliaManager.Data.Migrations
 {
     [DbContext(typeof(MemorabiliaManagerContext))]
-    partial class MemorabiliaManagerContextModelSnapshot : ModelSnapshot
+    [Migration("20220327230038_ItemFKFix")]
+    partial class ItemFKFix
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- MemorabiliaManager.Domain/Item.cs
+++ MemorabiliaManager.Domain/Item.cs
@@ -5,7 +5,7 @@
 		public int Id { get; set; }
 		public Entertainment Entertainment { get; set; } = new Entertainment();
 		public List<ItemSubject>? Subjects { get; set; } = null;
-		public Signature? Signature { get; set; } = null;
+		public List<ItemSignature>? Signatures { get; set; } = null;
 		public ItemCategory ItemCategory { get; set; } = new ItemCategory();
 		public string Name { get; set; } = string.Empty;
 		public string? ItemCollectionNumber { get; set; } = null;
