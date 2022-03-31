b692f37722af0bce433bd559179e30f2cdff49e3
Rick B <SBoarder@protonmail.com.com>
2022-03-30 20:03:42 -0500

    Removed Auth column, created dedicated Authentication table

--- MemorabiliaManager.Domain/Signature.cs
+++ MemorabiliaManager.Domain/Signature.cs
@@ -13,8 +13,6 @@ namespace MemorabiliaManager.Domain
 		public string LastName { get; set; } = string.Empty;
 		[Range(1, 99)]
 		public short? JerseyNumber { get; set; } = null;
-		[StringLength(15)]
-		public string? AuthenticationNumber { get; set; } = null;
 		public Entertainment Entertainment { get; set; } = new Entertainment();
 		public List<ItemSignature> Items { get; set; } = new List<ItemSignature>();
 		public int EntertainmentId { get; set; } = 0;
