ce0d9d10bb39866443012bc9059f559c2b73a646
Rick B <SBoarder@protonmail.com.com>
2022-03-30 20:24:28 -0500

    Initialized main form

---
+++ MemorabiliaManager.UI/FrmMainMenu.cs
@@ -0,0 +1,40 @@
+namespace MemorabiliaManager
+{
+	public partial class FrmMainMenu : Form
+	{
+		public FrmMainMenu()
+		{
+			InitializeComponent();
+		}
+
+		private void btnViewItems_Click(object sender, EventArgs e)
+		{
+			MessageBox.Show("Not implemented yet", "Pending", MessageBoxButtons.OK, MessageBoxIcon.Error);
+		}
+
+		private void btnViewSignatures_Click(object sender, EventArgs e)
+		{
+			MessageBox.Show("Not implemented yet", "Pending", MessageBoxButtons.OK, MessageBoxIcon.Error);
+		}
+
+		private void btnMaintenance_Click(object sender, EventArgs e)
+		{
+			MessageBox.Show("Not implemented yet", "Pending", MessageBoxButtons.OK, MessageBoxIcon.Error);
+		}
+
+		private void btnAddItem_Click(object sender, EventArgs e)
+		{
+			MessageBox.Show("Not implemented yet", "Pending", MessageBoxButtons.OK, MessageBoxIcon.Error);
+		}
+
+		private void btnAddSignature_Click(object sender, EventArgs e)
+		{
+			MessageBox.Show("Not implemented yet", "Pending", MessageBoxButtons.OK, MessageBoxIcon.Error);
+		}
+
+		private void btnAddSignatureUploadImage_Click(object sender, EventArgs e)
+		{
+			MessageBox.Show("Not implemented yet", "Pending", MessageBoxButtons.OK, MessageBoxIcon.Error);
+		}
+	}
+}
